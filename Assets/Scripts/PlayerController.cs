37744ebebf06eb0f066777cea99cbbd534abc7a4
ozstudio <87773911+ozstudio@users.noreply.github.com>
2023-06-16 18:28:14 +0300

    Changing speed

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,38 +1,143 @@
-using System.Collections;
-using System.Collections.Generic;
+using UniRx;
 using UnityEngine;
 
-[RequireComponent(typeof(Rigidbody),typeof(SphereCollider))]
+[RequireComponent(typeof(Rigidbody), typeof(SphereCollider))]
 public class PlayerController : MonoBehaviour
 {
     [SerializeField]
     private Rigidbody _rigidbody;
+
     [SerializeField]
     private FixedJoystick _fixedJoystick;
+
     [SerializeField]
+    float _speedIncreaseFactor = 1f;
+    [SerializeField]
+    Transform _raycastPoint;
+    [SerializeField]
+    float _minDistanceToGround = 0.1f;
+
     private float _moveSpeed;
-    [SerializeField]
-    private float _jumpForce;
+    private float _maxSpeed;
+    private float _minSpeed;
+    private float _currentTemp;
+    private float _prevTemp;
+
+
+
+    public float maxHeight = 3f;
+    public BlobView blobV;
+
+
+    private void Start()
+    {
+
+        _maxSpeed = GlobalModel.Instance.StartBlobSpeed * 4;
+        _minSpeed = GlobalModel.Instance.StartBlobSpeed / 2;
+        _moveSpeed = GlobalModel.Instance.StartBlobSpeed;
+        _currentTemp = GlobalModel.Instance.StartBlobTemp;
+        _prevTemp = _currentTemp;
+
+        Invoke("GetBlowV", 1f);
+    }
+
+
+    void GetBlowV()
+    {
+        blobV.blobTempViewModel.blobTemp.Subscribe(blobTemp =>
+        {
+            _currentTemp = blobTemp;
+
+            if (_currentTemp > _prevTemp)
+            {
+                _moveSpeed += _speedIncreaseFactor;
+            }
+
+            if (_currentTemp < _prevTemp)
+            {
+                _moveSpeed -= _speedIncreaseFactor;
+
+            }
+
+        });
+    }
 
 
     private void FixedUpdate()
     {
-        _rigidbody.velocity = 
-           new  Vector3(_fixedJoystick.Horizontal
-           * _moveSpeed,_rigidbody.velocity.y,0);
+        print(_moveSpeed);
+        if (_fixedJoystick.Horizontal > 0)
+        {
+
+            _rigidbody.velocity =
+           new Vector3(1 * BlobSpeed(), _rigidbody.velocity.y, 0);
+        }
+        if (_fixedJoystick.Horizontal < 0)
+        {
+            _rigidbody.velocity =
+          new Vector3(-1 * BlobSpeed(), _rigidbody.velocity.y, 0);
+
+        }
+
+    }
+
+    float BlobSpeed()
+    {
+
+        if (_moveSpeed >= _maxSpeed)
+        {
+            _moveSpeed = _maxSpeed;
+        }
+
+        if (_moveSpeed <= _minSpeed)
+        {
+            _moveSpeed = _minSpeed;
+        }
+
+        return _moveSpeed;
+
     }
     private void Update()
     {
-        
-        if(Input.GetKeyDown(KeyCode.Space))
+        // print(_minSpeed + ":" + _moveSpeed);
+        if (Input.GetKeyDown(KeyCode.Space))
         {
-            
             Jump();
         }
     }
 
     public void Jump()
     {
-        _rigidbody.AddForce(_jumpForce * Vector3.up,ForceMode.Force);
+
+        if (CheckIfGrounded())
+        {
+            float jumpHeight = maxHeight * gameObject.transform.localScale.x;
+            _rigidbody.AddForce(Vector3.up * Mathf.Sqrt(jumpHeight * -2 * Physics.gravity.y), ForceMode.VelocityChange);
+        }
+
     }
+
+    bool CheckIfGrounded()
+    {
+        bool grounded = false;
+        RaycastHit hit;
+        Ray ray = new Ray(_raycastPoint.position, -transform.up);
+        Debug.DrawRay(_raycastPoint.position, -transform.up, Color.red);
+
+        if (Physics.Raycast(ray, out hit, _minDistanceToGround))
+        {
+
+            if (hit.collider != null)
+            {
+                grounded = true;
+            }
+            else
+            {
+                grounded = false;
+            }
+        }
+        return grounded;
+    }
+
+
 }
